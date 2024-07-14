e8365ebade1d5be466e30c578ccb38715398682d
MinhKQ <minhkieu702@gmail.com>
2024-07-14 19:40:21 +0700

    done asignment 4

--- Assignment/Assignment4_WindowsService/Worker.cs
+++ Assignment/Assignment4_WindowsService/Worker.cs
@@ -1,23 +1,33 @@
-using Data.Models;
-using Services;
-using System.Net.Http;
 using System.Text.Json;
 
 namespace Assignment4_WindowsService
 {
+    public partial class Order
+    {
+        public int OrderId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string Type { get; set; } = null!;
+
+        public double TotalAmount { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public string? OrderNotes { get; set; }
+        public bool? Delete { get; set; }
+    }
     public class Worker : BackgroundService
     {
         private readonly HttpClient _httpClient;
         private readonly ILogger<Worker> _logger;
-        private readonly Service _service;
         private const int ThreadDelay = 5000;
         private readonly string FilePath = @"D:\now_semester\PRN221\AssignmentPRN221\Logging.txt";
-
+        private readonly string jsonFilePath = "D:\\now_semester\\PRN221\\AssignmentPRN221\\Assignment\\Assignment4_WindowsService\\Orders.json";
         public Worker(ILogger<Worker> logger, HttpClient httpClient)
         {
             _httpClient = httpClient;
             _logger = logger;
-            _service ??= new Service();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -56,12 +66,39 @@ namespace Assignment4_WindowsService
 
         public void DeleteWithDeleteProp(out List<Order> ordersDeleteList, out List<Order> ordersExistList)
         {
-            _service.ChangeSource("Json");
-            var orders = _service.GetAllOrders();
+            var orders = LoadOrdersFromJson();
             ordersDeleteList = orders.Where(b => b.Delete == true).ToList();
             ordersExistList = orders.Where(b => b.Delete == false).ToList();
-            orders.Where(b => b.Delete.Value).ToList().ForEach(o => _service.Delete(o.OrderId));
-            _service.SaveChange("Json");
+            orders.Where(b => b.Delete.Value).ToList().ForEach(o => orders.Remove(o));
+            SaveOrdersToJson(orders);
+        }
+
+        private List<Order> LoadOrdersFromJson()
+        {
+            try
+            {
+                string jsonString = File.ReadAllText(jsonFilePath);
+                return JsonSerializer.Deserialize<List<Order>>(jsonString);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private bool SaveOrdersToJson(List<Order> orders)
+        {
+            try
+            {
+                string jsonString = JsonSerializer.Serialize(orders, new JsonSerializerOptions { WriteIndented = true });
+
+                File.WriteAllText(jsonFilePath, jsonString);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
